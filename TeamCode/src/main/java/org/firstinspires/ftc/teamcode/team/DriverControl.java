0dcfd6cfc8a5bf103c2da5c172ec7408179b0f35
Justin <112041049+justinmosier@users.noreply.github.com>
2025-04-26 12:45:16 -0500

    WIP We are workin going on raising the arm and moving the robot simultaneously

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/DriverControl.java
@@ -19,9 +19,24 @@ public class DriverControl extends DarienOpModeTeleop {
 
             telemetry.update();
 
+            if (gamepad1.a){
+                scoringLowBasket(0.1, [0,0.1]);
+                
+
+            }
 
             //runMacro("ReadyToPickup");
             //runMacro("ReadyToDrop");
         }
+    }
+
+    public void scoringLowBasket(double slideMotor1Power, double[] MoveRobotPower) {
+         slideMotor1.setPower(slideMotor1Power);
+         MoveRobot(MoveRobotPower,0,0);
+
+         
+         // if the robot reaches the b positions for both the arm and the position, stop moving
+         // for positions, robot will move for 10s, arm will move after 2s and end 2s before the 10s is finished (2s and 8s)
+         //after get both arm and position to point b and doing it's task, then it will loop back to the beginning of the code and rerun the entire code
     }
 }
