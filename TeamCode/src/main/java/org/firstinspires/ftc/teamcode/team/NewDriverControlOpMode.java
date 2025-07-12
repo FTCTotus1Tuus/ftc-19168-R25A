194e12f7490aa1439763b114f5279ea123a5f92c
FTCTutus1Tuus <ftc19168@gmail.com>
2025-07-12 12:35:27 -0500

    Finished universal PID function tuned tilt motor cleaned up code create a new class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/NewDriverControlOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/NewDriverControlOpMode.java
@@ -7,19 +7,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp//(name="DC", group="DriverControl")
 @Config
 public class NewDriverControlOpMode extends DarienOpModeTeleop {
-    public static double tilt_pgain = .03;
+    public static double tilt_pgain = .01;
     public static double tilt_igain = .0003;
-    public static double tilt_gain = 1;
-    public static double T2 = 650;
-    public static double T1 = 20;
+    public static double tilt_gain = 7;
+    public static double T2 = 1750;
+    public static double T1 = 10;
+    private MotorHelper tiltMotorHelper;
+    private MotorHelper slideMotorHelper;
 
     @Override
     public void runOpMode() {
         initControls();
         waitForStart();
         //Start
-        double tilt_power = 0, tilt_iduty = 0, tilt_pduty = 0;
-        double tilt_sp = T1;
+        double[] tiltMotorPID = {0, 0, 0, 0};
+        tiltMotorHelper = new MotorHelper(telemetry);
+        slideMotorHelper = new MotorHelper(telemetry);
 
         while (this.opModeIsActive()) {
             runClaw();
@@ -27,27 +30,18 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
             runDriveSystem();
             //runSlideMotorSystem();
             runMotorWithEncoderStops(slideMotor1, gamepad2.right_stick_y,"Slide",0.6, 0, 100, 3900, 100);
-           // runMotorWithEncoderStops(tiltMotor, gamepad2.left_stick_y,"Tilt" ,-1, 0, 100, 2300, 100);
-
-            //-25 to 440
-           //tilt_sp = clamp(gamepad2.left_stick_y * -1, 0.0, 1.0) * (T2-T1) + T1;
-
-            tilt_sp = clamp(tilt_sp - gamepad2.left_stick_y * tilt_gain, T1, T2);
-
-            //tilt_iduty = tilt_power;
-            tilt_pduty = clamp(tilt_pgain * (tilt_sp - tiltMotor.getCurrentPosition()), -.5, 0.8);
-            tilt_iduty = clamp(tilt_igain * (tilt_sp - tiltMotor.getCurrentPosition()) + tilt_iduty, -0.2, 1);
-            tilt_power = clamp(tilt_pduty + tilt_iduty, -0.5, 1.0);
-
-            tiltMotor.setPower(tilt_power);
-
-            telemetry.addData("Pduty: ", tilt_pduty);
-            telemetry.addData("Iduty: ", tilt_iduty);
-            telemetry.addData("tiltPos: ", tiltMotor.getCurrentPosition());
-            telemetry.addData("tiltsp: ", tilt_sp);
-            telemetry.update();
-
-
+            // runMotorWithEncoderStops(tiltMotor, gamepad2.left_stick_y,"Tilt" ,-1, 0, 100, 2300, 100);
+            tiltMotorPID = tiltMotorHelper.pid(
+                    tiltMotor,
+                    tilt_pgain,
+                    tilt_igain,
+                    tiltMotorPID[3], -.5, 0.8, -0.2, 1, tiltMotorPID[2],-0.5, 1,
+                    tilt_gain,
+                    T1,
+                    T2,
+                    gamepad2.left_stick_y
+            );
+            tiltMotor.setPower(tiltMotorPID[0]);
         }
     }
 }
