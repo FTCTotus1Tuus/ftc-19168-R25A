f8648f2f94939925d7d55f9f6cc6e1b564497beb
FTCTutus1Tuus <ftc19168@gmail.com>
2025-10-04 12:53:12 -0500

    Finished tray pos servo with 6 positions, slight problem due to the pos servo having a deadzone thus not being able to have the 3rd pos scoring available, 1i,1s,2i,2s,3i working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/NewDriverControlOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/NewDriverControlOpMode.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.team;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 
 
 @TeleOp//(name="DC", group="DriverControl")
@@ -13,6 +12,12 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
     // tuning constants for gobilda 312 rpm motor and 4 stage long gobilda viper slide
     public static double IntakeServoPosUp = 0.7;
     public static double IntakeServoPosDown = 0.2;
+    public static double TrayPos1Intake = 0;
+    public static double TrayPos2Intake = 0.885;
+    public static double TrayPos3Intake = 0.45;
+    public static double TrayPos1Score = 0.7;
+    public static double TrayPos2Score = 0.2;
+    public static double TrayPos3Score = 0;
 
     @Override
     public void runOpMode() {
@@ -39,7 +44,20 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
             if (gamepad2.a){
                 IntakeServo.setPosition(IntakeServoPosUp);
             } else {
-                IntakeServo.setPosition((IntakeServoPosDown));
+                IntakeServo.setPosition(IntakeServoPosDown);
+            }
+            if (gamepad2.dpad_left && gamepad2.y){
+                Tray.setPosition(TrayPos1Intake);
+            } else if (gamepad2.dpad_left && gamepad2.x){
+                Tray.setPosition(TrayPos1Score);
+            } else if (gamepad2.dpad_up && gamepad2.y){
+                Tray.setPosition(TrayPos2Intake);
+            } else if (gamepad2.dpad_up && gamepad2.x){
+                Tray.setPosition(TrayPos2Score);
+            } else if (gamepad2.dpad_right && gamepad2.y){
+                Tray.setPosition(TrayPos3Intake);
+            } else if (gamepad2.dpad_right && gamepad2.x){
+                Tray.setPosition(TrayPos3Score);
             }
 
         }
