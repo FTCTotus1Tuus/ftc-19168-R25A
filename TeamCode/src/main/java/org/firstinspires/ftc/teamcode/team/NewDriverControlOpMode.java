1ed29f97e2991d9096f2592a7694bb8fc882c0c5
FTCTutus1Tuus <ftc19168@gmail.com>
2025-10-11 11:53:28 -0500

    Working intake New function to slowdown intake servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/NewDriverControlOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/NewDriverControlOpMode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.team;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp//(name="DC", group="DriverControl")
@@ -10,17 +11,18 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
     // tuning constants for gobilda 117 rpm motor
 
     // tuning constants for gobilda 312 rpm motor and 4 stage long gobilda viper slide
-    public static double IntakeServoPosUp = 0.7;
-    public static double IntakeServoPosDown = 0.2;
-    public static double TrayPos1Intake = 0.4;
-    public static double TrayPos2Intake = 0;
-    public static double TrayPos3Intake = 0.8;
-    public static double TrayPos1Score = 1;
-    public static double TrayPos2Score = 0.6;
-    public static double TrayPos3Score = 0.2;
+    public static double INTAKE_SERVO_POS_UP = 0.67;
+    public static double INTAKE_SERVO_POS_DOWN = 0.21;
+    public static double TRAY_POS_1_INTAKE = 0.4;
+    public static double TRAY_POS_2_INTAKE = 0;
+    public static double TRAY_POS_3_INTAKE = 0.8;
+    public static double TRAY_POS_1_SCORE = 1;
+    public static double TRAY_POS_2_SCORE = 0.6;
+    public static double TRAY_POS_3_SCORE = 0.2;
     double IntakeServoPosition = 0;
     double startTimeIntakeServo = 0;
     boolean isIntakeServoMoving = false;
+
 
     public double getIntakeServoPosition() {
         return IntakeServoPosition;
@@ -29,6 +31,27 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
         IntakeServo.setPosition(position);
         IntakeServoPosition = position;
     }
+    public void servoIncremental(Servo servo, double endPos, double startPos, double endDuration) {
+        //calculate how many increments it will take to reach to position in the target time
+        double currentPos;
+        double startTime = getRuntime();
+        double currentTime = startTime;
+        while (currentTime - startTime < endDuration) {
+            currentPos = ((endPos - startPos) / (endDuration - (currentTime - startTime))) * (currentTime - startTime) + startPos;
+            servo.setPosition(currentPos);
+            telemetry.addData("currentPos:", currentPos);
+            telemetry.addData("currentTime:", currentTime);
+
+            if (currentPos >= endPos){
+                sleep(1000);
+                break;
+            }
+            currentTime = getRuntime();
+
+        }
+        sleep(3000);
+    }
+
 
     @Override
     public void runOpMode() {
@@ -48,6 +71,7 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
                 ejectionMotorRight.setPower(0);
             }
 
+            /* TODO: DISABLE SPINNER UNTIL WE SWITCH IT TO A POSITIONAL SERVO
             // CONTROL: SPINNER
             if (gamepad2.left_bumper){
                 Spinner.setPower(1);
@@ -55,7 +79,17 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
                 Spinner.setPower(0);
             }
 
+             */
+
             // CONTROL: INTAKE
+            //classify the function
+            //when g2.a button is pressed intake servo goes up in increments in relation to the time
+            if (gamepad2.a){
+                servoIncremental(IntakeServo, INTAKE_SERVO_POS_UP, INTAKE_SERVO_POS_DOWN, 1);
+            } else {
+                IntakeServo.setPosition(INTAKE_SERVO_POS_DOWN);
+            }
+            /*
             if (isIntakeServoMoving) {
                 // Continue moving the servo until the position is reached.
                 if (getIntakeServoPosition() < IntakeServoPosUp && getRuntime()-startTimeIntakeServo < 4) {
@@ -72,20 +106,20 @@ public class NewDriverControlOpMode extends DarienOpModeTeleop {
                     IntakeServo.setPosition(IntakeServoPosDown);
                 }
             }
-
+            */
             // CONTROL: ROTATING TRAY
             if (gamepad2.dpad_left){
-                Tray.setPosition(TrayPos1Intake);
+                Tray.setPosition(TRAY_POS_1_INTAKE);
             } else if (gamepad2.x){
-                Tray.setPosition(TrayPos1Score);
+                Tray.setPosition(TRAY_POS_1_SCORE);
             } else if (gamepad2.dpad_up){
-                Tray.setPosition(TrayPos2Intake);
+                Tray.setPosition(TRAY_POS_2_INTAKE);
             } else if (gamepad2.y){
-                Tray.setPosition(TrayPos2Score);
+                Tray.setPosition(TRAY_POS_2_SCORE);
             } else if (gamepad2.dpad_right){
-                Tray.setPosition(TrayPos3Intake);
+                Tray.setPosition(TRAY_POS_3_INTAKE);
             } else if (gamepad2.b){
-                Tray.setPosition(TrayPos3Score);
+                Tray.setPosition(TRAY_POS_3_SCORE);
             }
 
         }
