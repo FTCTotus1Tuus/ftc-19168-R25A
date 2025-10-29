a3ee70ed6d5f49b3a5a191410b2685cb747b3113
FTCTutus1Tuus <ftc19168@gmail.com>
2025-10-28 20:53:24 -0500

    WIP BlueGoalSide1. Collab

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/BlueGoalSide1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/BlueGoalSide1.java
@@ -13,30 +13,82 @@ public class BlueGoalSide1 extends DarienOpModeAuto {
         initControls();
         waitForStart();
         if (isStopRequested()) return;
-
         //resetting all positions
         Elevator.setPosition(ELEVATOR_POS_DOWN);
         Feeder.setPosition(FEEDER_POS_DOWN);
         servoIncremental(TrayServo,TRAY_POS_2_SCORE,currentTrayPosition, 1,4);
         currentTrayPosition = TRAY_POS_2_SCORE;
 
-        //shoot artifact 1
+        //move backwards to read obelisk
+        moveXY(-26, 0, .25);
+        waitForMotors(true);
+        //sleep so cam can read apriltag
+        sleep(2000);
+        encoderRotate(Math.toRadians(-65),.5,true);
+        waitForMotors(true);
+
+        //shoot aritfact 1
+        //displayTrayTelemetry();
         Elevator.setPosition(ELEVATOR_POS_UP);
-        sleep(100);
-        shotGun(SHOT_GUN_POWER_DOWN);
+        //shotGun(SHOT_GUN_POWER_DOWN);
         //start spinning down
-        sleep(500);
-        shotGun(SHOT_GUN_POWER_UP_GOAL);
-        sleep(400);
+        sleep(100);
+        shotGun(SHOT_GUN_POWER_UP);
+        sleep(600);
         //start spinning up
         Feeder.setPosition(FEEDER_POS_UP);
         //move feeder up while spinner is still spinning
-        sleep(1500);
+        sleep(400);
         shotGunStop();
         //stop spinning
         Feeder.setPosition(FEEDER_POS_DOWN);
         Elevator.setPosition(ELEVATOR_POS_DOWN);
+        sleep(500);
+        //setBreakpoint();
 
+        //shoot artifact 2
+        //displayTrayTelemetry();
+        servoIncremental(TrayServo,TRAY_POS_3_SCORE,currentTrayPosition, 1, 4);
+        currentTrayPosition = TRAY_POS_3_SCORE;
+        sleep(1000);
+        //displayTrayTelemetry();
+        Elevator.setPosition(ELEVATOR_POS_UP);
+        //shotGun(SHOT_GUN_POWER_DOWN);
+        //start spinning down
+        sleep(100);
+        shotGun(SHOT_GUN_POWER_UP);
+        sleep(500);
+        //start spinning up
+        Feeder.setPosition(FEEDER_POS_UP);
+        //move feeder up while spinner is still spinning
+        sleep(400);
+        shotGunStop();
+        //stop spinning
+        Feeder.setPosition(FEEDER_POS_DOWN);
+        Elevator.setPosition(ELEVATOR_POS_DOWN);
+        sleep(500);
+        //setBreakpoint();
 
+        //shoot artifact 3
+        servoIncremental(TrayServo,TRAY_POS_1_SCORE,currentTrayPosition, 1,4);
+        currentTrayPosition = TRAY_POS_1_SCORE;
+        sleep(1000);
+        Elevator.setPosition(ELEVATOR_POS_UP);
+        //shotGun(SHOT_GUN_POWER_DOWN);
+        //start spinning down
+        sleep(100);
+        shotGun(SHOT_GUN_POWER_UP);
+        sleep(400);
+        //start spinning up
+        Feeder.setPosition(FEEDER_POS_UP);
+        //move feeder up while spinner is still spinning
+        sleep(500);
+        shotGunStop();
+        //stop spinning
+        Feeder.setPosition(FEEDER_POS_DOWN);
+        Elevator.setPosition(ELEVATOR_POS_DOWN);
+        sleep(1000);
+        //automaticIntake();
     }
 }
+
